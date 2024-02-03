4a606592ce1352f56c14daad658dd92ce56ffd0f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-03 19:46:28 +0300

    add build page

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+// components/About.tsx
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className='about-page'>
+      <h2>About</h2>
+      <p>This is the About page.</p>
+    </div>
+  );
+};
+
+export default About;
