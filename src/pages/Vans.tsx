a79b894275f83a6cc147ebe0b95f75ec3994730e
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-03 19:45:34 +0300

    add build page

---
+++ src/pages/Vans.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Vans: React.FC = () => {
+    return (
+        <div className="vans-page">
+            <h1>This is Vans pages</h1>
+        </div>
+    )
+}
+
+export default Vans;
