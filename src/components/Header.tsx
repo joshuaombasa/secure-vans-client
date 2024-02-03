0dbe75539b6a210cc0e308fd7b4c16738719ed13
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-03 19:48:33 +0300

    add build component

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header: React.FC = () => {
+    return (
+        <div className="header-container">
+            <Link to="/">VANLIFE</Link>
+            <div className="nav-links">
+                <NavLink 
+                    to="about"
+                >About</NavLink>
+                <NavLink 
+                    to="vans"
+                >Vans</NavLink>
+                <NavLink 
+                    to="vendors"
+                >Vendor</NavLink>
+                <NavLink 
+                    to="login"
+                >Login</NavLink>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header
