76ced1487116afeeae480f581d37b4a8ce6916cb
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-03 19:45:50 +0300

    add build page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Login: React.FC = () => {
+    return (
+        <div className="login-page">
+            <h1>Login page</h1>
+        </div>
+    )
+}
+
+export default Login;
