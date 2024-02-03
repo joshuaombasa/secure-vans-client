d5af45edf8193126dfcf69bc33bdf48fbd3706e7
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-03 19:48:03 +0300

    add build component

---
+++ src/components/VendorLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const VendorLayout: React.FC = () => {
+    return (
+        <div className="vendor-layout">
+            <h1>This is vendor layout</h1>
+        </div>
+    )
+}
+
+export default VendorLayout;
