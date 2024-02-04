5793944e42e34c68c8fccf0f99940e27530bbac0
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:49:19 +0300

    add build component

---
+++ src/components/ListedVansItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ListedVansItem: React.FC = () => {
+    return (
+        <Link to="1" className="listed-vans-item">
+            <img src={`https://assets.scrimba.com/advanced-react/react-router/green-wonder.png`} alt="" />
+            <div className="item-info">
+                <h3>Modest Explorer</h3>
+                <h4>$60/day</h4>
+            </div>
+        </Link>
+    )
+}
+
+export default ListedVansItem
