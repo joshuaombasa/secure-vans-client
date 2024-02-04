2d7da9c334beabbd37b28d275aa394b33f8b9c2c
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:48:20 +0300

    add build page

---
+++ src/pages/vendor/ListedVans.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ListedVansItem from "../../components/ListedVansItem";
+
+const ListedVans: React.FC = () => {
+    return (
+        <div className="listed-vans-page">
+            <h1>Your listed vans</h1>
+            <div className="listed-vans-parent">
+                <ListedVansItem/>
+                <ListedVansItem/>
+                <ListedVansItem/>
+                <ListedVansItem/>
+            </div>
+        </div>
+    )
+}
+export default ListedVans
