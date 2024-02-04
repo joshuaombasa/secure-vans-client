c3193a77abeca987f7ecea4848d70b63ca382f55
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:47:25 +0300

    update build page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-  return (
-    <div className='home-page'>
-      <h2>Home</h2>
-      <p>Welcome to the Home page!</p>
-    </div>
-  );
+    return (
+        <div className='home-page'>
+            <div className="home-page-content">
+                <h2>You got the travel plans, we got the travel vans.</h2>
+                <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+                <Link to="/vans">Find your van</Link>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
