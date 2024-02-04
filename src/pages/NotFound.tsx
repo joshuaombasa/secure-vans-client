a4e8ada220982e3a9cf81c63881aff962b90ed6b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:47:59 +0300

    add build page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+    return (
+        <div className="not-found-page">
+            <div className="not-found-content">
+                <h1>Sorry, the page you were looking for was not found.</h1>
+                <Link to="/">Return to home</Link>
+            </div>
+        </div>
+    )
+}
+
+
+export default NotFound
