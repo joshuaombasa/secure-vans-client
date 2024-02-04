e7a969717ac886eb61321a13868247622c2016b5
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:46:38 +0300

    update build page

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import VanCard from "../components/VanCard";
 
 
 const Vans: React.FC = () => {
     return (
         <div className="vans-page">
-            <h1>This is Vans pages</h1>
+            <div className="home-page-content">
+                <h1>Explore our van options</h1>
+                <div className="vans-parent">
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                    <VanCard/>
+                </div>
+            </div>
         </div>
     )
 }
