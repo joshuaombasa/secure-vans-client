cf1976def4c92763b26a9cff983b77a78e8d746c
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:49:09 +0300

    add build component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>Ⓒ 2024 #VANLIFE</p>
+        </footer>
+    )
+}
+
+export default Footer
