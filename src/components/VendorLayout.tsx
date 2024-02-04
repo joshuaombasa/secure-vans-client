30e335e57d7d7270967f8970e7ecac7812c1fc03
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:49:57 +0300

    update build component

--- src/components/VendorLayout.tsx
+++ src/components/VendorLayout.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Outlet } from "../../node_modules/react-router-dom/dist/index";
 
 
 
 const VendorLayout: React.FC = () => {
     return (
         <div className="vendor-layout">
-            <h1>This is vendor layout</h1>
+            <nav className="vendor-layout-nav">
+                <NavLink
+                    to="."
+                    // end={true}
+                >Dashboard</NavLink>
+                <NavLink
+                    to="income"
+                >Income</NavLink>
+                <NavLink
+                    to="vans"
+                >Vans</NavLink>
+                <NavLink
+                    to="reviews"
+                >Reviews</NavLink>
+            </nav>
+            <Outlet/>
         </div>
     )
 }
