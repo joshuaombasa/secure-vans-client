79750097b2b38c4799732351ffe7ce06c8cb373a
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:48:39 +0300

    add build page

---
+++ src/pages/vendor/Income.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Income: React.FC = () => {
+    return (
+        <div className="">
+            <h1>This is Income page</h1>
+            
+        </div>
+    )
+}
+export default Income
