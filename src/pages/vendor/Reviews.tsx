aab116e0afd25744627beeced6123d5f5de55470
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:48:09 +0300

    add build page

---
+++ src/pages/vendor/Reviews.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reviews: React.FC = () => {
+    return (
+        <div className="">
+            <h1>This is reviews page</h1>
+        </div>
+    )
+}
+
+export default Reviews
