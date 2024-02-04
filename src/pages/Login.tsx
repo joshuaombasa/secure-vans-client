90bcc0088d28ece6426ade8864651d2d3c402bff
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-04 22:46:49 +0300

    update build page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Login: React.FC = () => {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const {name, value}  = event.target
+        setFormData(prevState => (
+            {...prevState,
+            [name]: value}
+        ))
+    }
+
     return (
         <div className="login-page">
-            <h1>Login page</h1>
+            <div className="login-page-content">
+                <h1>Sign in to your account</h1>
+                <form>
+                    <div className="inputs">
+                        <input
+                            type="text"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="email"
+                        />
+                        <input
+                            type="text"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            className="password"
+                        />
+                    </div>
+                    <button className="login-button">Sign in</button>
+                </form>
+                <span>Don’t have an account? 
+                    <Link to="signup" className="signup-link">Create one now</Link>
+                </span>
+            </div>
         </div>
     )
 }
