7aefad1ace682b94f13713f3feec130324bcdffd
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-05 23:29:36 +0300

    add return type to build component

--- src/components/ListedVansItem.tsx
+++ src/components/ListedVansItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ListedVansItem: React.FC = () => {
+const ListedVansItem: React.FC = () : JSX.Element => {
     return (
         <Link to="1" className="listed-vans-item">
             <img src={`https://assets.scrimba.com/advanced-react/react-router/green-wonder.png`} alt="" />
