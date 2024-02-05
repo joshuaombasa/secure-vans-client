fd1fc8e447938755f6f3fab292f618e4969ce58b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-05 23:30:50 +0300

    add return type to build component

--- src/pages/vendor/Reviews.tsx
+++ src/pages/vendor/Reviews.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Reviews: React.FC = () => {
+const Reviews: React.FC = () : JSX.Element => {
     return (
         <div className="">
             <h1>This is reviews page</h1>
