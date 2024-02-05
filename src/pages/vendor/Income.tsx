cb0681d000b9344fc27c9720230c8fb42dafb4f3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-05 23:30:18 +0300

    add return type to build component

--- src/pages/vendor/Income.tsx
+++ src/pages/vendor/Income.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Income: React.FC = () => {
+const Income: React.FC = () : JSX.Element => {
     return (
         <div className="">
             <h1>This is Income page</h1>
